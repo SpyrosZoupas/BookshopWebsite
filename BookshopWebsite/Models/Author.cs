73b84d3728e41230efd97ca04ca47487d84803f6
Spiros <spiroszoupas@hotmail.com>
2023-08-12 17:36:51 +0100

    Create Book,Category,Author model classes.

---
+++ BookshopWebsite/Models/Author.cs
@@ -0,0 +1,15 @@
+﻿namespace BookshopWebsite.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public string? Nationality { get; set; }
+        public int Age { get; set; }
+        public string? Birthday { get; set; }
+        public string? Deathday { get; set; }
+        public List<Book>? Books { get; set; }
+
+    }
+}
---
+++ BookshopWebsite/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿namespace BookshopWebsite.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public string? NumberOfPages { get; set; }
+        public string? ReleaseDate { get; set; }
+        public decimal Price { get; set; }
+        public bool InStock { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } = default!;
+        public int AuthorId { get; set; }
+        public Author Author { get; set; } = default!;
+
+    }
+}
