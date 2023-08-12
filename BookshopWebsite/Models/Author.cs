543b15877ddd5467a1704a9441075caa50736a91
Spiros <spiroszoupas@hotmail.com>
2023-08-12 18:14:13 +0100

    change "Id" property to "AuthorId"

--- BookshopWebsite/Models/Author.cs
+++ BookshopWebsite/Models/Author.cs
@@ -2,7 +2,7 @@
 {
     public class Author
     {
-        public int Id { get; set; }
+        public int AuthorId { get; set; }
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
         public string? Nationality { get; set; }
