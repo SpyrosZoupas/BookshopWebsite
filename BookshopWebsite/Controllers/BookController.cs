6b811e945f3dd195df176abb79237dff1a2c42d4
Spiros <spiroszoupas@hotmail.com>
2023-08-16 15:55:00 +0100

    book controller has "Book" action that returns all books to the view

---
+++ BookshopWebsite/Controllers/BookController.cs
@@ -0,0 +1,21 @@
+﻿using BookshopWebsite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookshopWebsite.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BookController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        public IActionResult Books()
+        {
+            var books = _bookRepository.GetBooks();
+            return View(books);
+        }
+    }
+}
